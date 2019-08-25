ff17de974aafd1836cc11d83947a1ed29cc5d023
Martynas Dapkus <martynasdapkus94@gmail.com>
2019-08-25 18:30:30 +0300

    added algorithm to create a board, place mines and count numbers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,140 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isExposed: false,
+            value: ''
+        };
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    render() {
+        return (
+            <button
+                className={`square ${this.state.isExposed ? 'exposed' : ''}`}
+                onClick={() => this.setState({isExposed: true})}
+                >
+                {/*{this.state.isExposed ? this.props.value : ''}*/}
+                {this.props.isBomb ? '\u2714' :  ''}
+                {this.props.number > 0 &&  !this.props.isBomb?  this.props.number : ''}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			rows: 10,
+			columns: 10,
+            bombs: 10
+		};
+	}
+	
+	renderSquare(i, isBomb, number) {
+		return <Square
+            key={i}
+            value={i}
+            isBomb={isBomb}
+            number={number}/>;
+	}
+
+	render() {
+        const board = [];
+        //make board and place bombs?
+        //place bombs atskritai padaryti?
+        var bombsToPlace = this.state.bombs;
+        for (var i = 0; i < this.state.rows; i++) {
+            const row = [];
+            for (var j = 0; j < this.state.columns; j++) {
+                row.push(
+                    {
+                        row: i,
+                        column: j,
+                        isBomb: bombsToPlace > 0,
+                        number: 0
+                    }
+                );
+                bombsToPlace--;
+            }
+            board.push(row);
+        }
+
+        //shuffle
+        board.map((row, rowIndex) => {
+            row.map((square, columnIndex) => {
+                var newRowIndex = Math.floor(Math.random() * this.state.rows);
+                var newColumnIndex = Math.floor(Math.random() * this.state.columns);
+                var tempSquareCopy1 = board[newRowIndex][newColumnIndex];
+
+                tempSquareCopy1.row = square.row;
+                tempSquareCopy1.column = square.column;
+
+                square.row = newRowIndex;
+                square.column = newColumnIndex;
+
+                board[rowIndex][columnIndex] = tempSquareCopy1;
+                board[newRowIndex][newColumnIndex] = square;
+            });
+        });
+
+        //shuffle
+        const numberOffsets = [
+            {x:-1, y:1},  {x:0, y:1},  {x:1, y:1},
+            {x:-1, y:0},               {x:1, y:0},
+            {x:-1, y:-1}, {x:0, y:-1}, {x:1, y:-1}
+        ];
+        board.map((row) => {
+            row.map((square) => {
+                if (square.isBomb) {
+                    numberOffsets.map((numberOffset) => {
+                        if (typeof board[square.row + numberOffset.x] !== 'undefined'
+                            && typeof board[square.row + numberOffset.x][square.column + numberOffset.y] !== 'undefined') {
+                            board[square.row + numberOffset.x][square.column + numberOffset.y].number =
+                                board[square.row + numberOffset.x][square.column + numberOffset.y].number + 1;
+                        }
+                    });
+                }
+            });
+        });
+        
+        const dataRow = board.map((row, rowIndex) => {
+            return (
+                <div key={rowIndex}>
+                    {
+                        row.map((square, columnIndex) => {
+                            return (
+                                this.renderSquare(square.row * 10 + square.column, square.isBomb, square.number)
+                            );
+                        })
+                    }
+                </div>
+            )
+        });
+
+		return (
+            <div>{dataRow}</div>
+		);
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
